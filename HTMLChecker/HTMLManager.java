b04e506651d7d61806873c03ee4484db76823a63
ThomasPK <thomasvanlucky@gmail.com>
2026-05-20 11:07:36 -0700

    Made the HTMLManager() Method, fixing the bugs

--- HTMLChecker/HTMLManager.java
+++ HTMLChecker/HTMLManager.java
@@ -3,4 +3,14 @@ import java.util.*;
 public class HTMLManager {
    private Queue<HTMLTag> tags;
    
+   private void HTMLManager(Queue<HTMLTag> html) {
+      if(html == null) {
+         throw new IllegalArgumentException();
+      } else {
+         this.html = new LinkedList<>();
+         for(HTMLTag tag : html) {
+            this.html.add(tag);
+         }
+      }
+   }
 }
