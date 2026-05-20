17d325026a5fa31b5c098aad06219a265cea2bff
ThomasPK <thomasvanlucky@gmail.com>
2026-05-20 11:15:43 -0700

    Fixed more bugs in the constructor

--- HTMLChecker/HTMLManager.java
+++ HTMLChecker/HTMLManager.java
@@ -3,12 +3,12 @@ import java.util.*;
 public class HTMLManager {
    private Queue<HTMLTag> tags;
    
-   private void HTMLManager(Queue<HTMLTag> html) {
+   public HTMLManager(Queue<HTMLTag> html) {
       if(html == null) {
          throw new IllegalArgumentException();
       } else {
          this.tags = new LinkedList<>();
-         for(HTMLTag tag : tags) {
+         for(HTMLTag tag : html) {
             this.tags.add(tag);
          }
       }
